db4f8c827585aa014989b5c76c5a2a7735c6b853
Tanmay Jadhav <tanmayjadhav2529@gmail.com>
2026-09-09 01:08:33 +0530

    Update analytics and reporting

--- app_backend/routers/analytics.py
+++ app_backend/routers/analytics.py
@@ -22,47 +22,6 @@ def analytics_summary():
 def analytics_defects():
     return get_defect_distribution()
 
-def get_inspection_trends():
-    inspections = get_all_inspections()
-
-    daily_data = {}
-
-    for inspection in inspections:
-        timestamp = inspection.get("timestamp")
-
-        if not timestamp:
-            continue
-
-        date = timestamp[:10]
-
-        if date not in daily_data:
-            daily_data[date] = {
-                "total": 0,
-                "good": 0,
-                "bad": 0
-            }
-
-        daily_data[date]["total"] += 1
-
-        if inspection.get("verdict") == "GOOD":
-            daily_data[date]["good"] += 1
-
-        elif inspection.get("verdict") == "BAD":
-            daily_data[date]["bad"] += 1
-
-    daily_inspections = []
-
-    for date, data in sorted(daily_data.items()):
-        daily_inspections.append({
-            "date": date,
-            "total": data["total"],
-            "good": data["good"],
-            "bad": data["bad"]
-        })
-
-    return {
-        "daily_inspections": daily_inspections
-    }
 
 @router.get("/trends")
 def analytics_trends():
